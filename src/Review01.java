7ad12abf031bc14b1d673ef7b73f7134e5805f01
19kyosuke37 <19kyosuke37@gmail.com>
2023-11-21 16:58:12 +0900

    taxメソッド訂正いたしました。

--- src/Review01.java
+++ src/Review01.java
@@ -3,25 +3,27 @@ public class Review01 {
 
     public static void main(String[] args) {
         // TODO 自動生成されたメソッド・スタブ
-        
+
         int cost = 1500;
         // cost は原価です。
+
         
-        int taxIn = tax(cost);
-        // taxIn　は税込み価格です。
-        
-        int result = (taxIn - cost);
+        int result = tax(cost);
         // result は消費税です。
         
+        int taxIn = (cost + result);
+        // taxIn　は税込み価格です。
+
+
         System.out.println(cost + "円の商品の税込価格は" + taxIn + "円"
     + "（消費税は" + result + "円)です。");
     }
-    
+
     public static int tax (int cost) {
-        
-        int result = ((int)(cost*1.1));
+
+        int result = ((int)(cost*0.1));
         return result;
-        
+
     }
 
 }
